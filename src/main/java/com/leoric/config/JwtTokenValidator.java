f81dd6b27052501827c51283f731d7927de871e0
Leoric01 <troglodit.nick@seznam.cz>
2024-10-08 18:36:28 +0200

    asf

---
+++ src/main/java/com/leoric/config/JwtTokenValidator.java
@@ -0,0 +1,21 @@
+package com.leoric.config;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtTokenValidator extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+            @NotNull HttpServletRequest request,
+            @NotNull HttpServletResponse response,
+            @NotNull FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
---
+++ src/test/java/com/leoric/ProjectmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leoric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmanagementsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
