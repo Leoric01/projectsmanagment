b5290c8d85f366ca11a17317538b810167f5e559
Leoric01 <troglodit.nick@seznam.cz>
2024-10-11 02:19:02 +0200

    Message fix - format and immutable field createdAt

--- src/main/java/com/leoric/models/Message.java
+++ src/main/java/com/leoric/models/Message.java
@@ -1,9 +1,11 @@
 package com.leoric.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
 
@@ -18,6 +20,9 @@ public class Message {
 
     private String content;
 
+    @JsonFormat(pattern = "yyyy-MM-dd' 'HH:mm:ss")
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
     private LocalDateTime createdAt;
 
     @ManyToOne
